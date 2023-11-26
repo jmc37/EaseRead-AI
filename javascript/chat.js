323659ccdf1bc43df73fdc0950be5b7cfd4b4bd7
B-Rabbit7 <snouri3@my.bcit.ca>
2023-11-25 23:35:13 -0800

    brought functions over

--- javascript/chat.js
+++ javascript/chat.js
@@ -5,52 +5,88 @@ const processing_error = chatStrings.messages.processingError;
 const load = chatStrings.load;
 const loading_wheel = chatStrings.bigHTML;
 
+// Function to get the value of a cookie by name
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
+
+function checkAdminAccess() {
+  const jwtToken = getCookie("access_token");
+
+  if (jwtToken) {
+    // Send a request to your server to validate the token
+    fetch("https://easeread-ai-backend.onrender.com/API/v1/admin-dashboard", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.is_admin) {
+          document.getElementById("adminButton").style.display = "block";
+        }
+      })
+      .catch((error) => {
+        console.error("Error checking admin access:", error);
+      });
+  }
+}
+
 // window.addEventListener(load, function () {
 //     checkAdminAccess();
 // });
 
 async function submitForm(event) {
-    event.preventDefault();
-    console.log("Form was submitted")
-    let question = document.getElementById("question").value;
+  event.preventDefault();
+  console.log("Form was submitted");
+  let question = document.getElementById("question").value;
 
-    // Create the request data object with user input
-    let requestData = {
-        question: question,
-    };
+  // Create the request data object with user input
+  let requestData = {
+    question: question,
+  };
 
-    // Display the loading Hamster spinner
-    document.getElementById("result").innerHTML = loading_wheel;
+  // Display the loading Hamster spinner
+  document.getElementById("result").innerHTML = loading_wheel;
 
-    try {
-        console.log("sending req")
-        // Make the POST request to your Flask backend
-        const response = await fetch(chat_route, {
-            method: post_method,
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(requestData),
-        });
+  try {
+    console.log("sending req");
+    // Make the POST request to your Flask backend
+    const response = await fetch(chat_route, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestData),
+    });
 
-        // Check if the request was successful (status code 200)
-        if (response.ok) {
-            const result = await response.json();
+    // Check if the request was successful (status code 200)
+    if (response.ok) {
+      const result = await response.json();
 
-            // Display the result in the frontend
-            const resultContainer = document.getElementById("result");
+      // Display the result in the frontend
+      const resultContainer = document.getElementById("result");
 
-            resultContainer.innerHTML = result.answer;
+      resultContainer.innerHTML = result.answer;
 
-            // Show the result container
-            document.getElementById("resultContainer").classList.remove('hidden');
-        } else {
-            // Handle non-successful response (e.g., show an error message)
-            console.error(processing_error, response.statusText);
-        }
-    } catch (error) {
-        console.error(processing_error, error);
-        // Handle processing error, e.g., show an error message
+      // Show the result container
+      document.getElementById("resultContainer").classList.remove("hidden");
+    } else {
+      // Handle non-successful response (e.g., show an error message)
+      console.error(processing_error, response.statusText);
     }
+  } catch (error) {
+    console.error(processing_error, error);
+    // Handle processing error, e.g., show an error message
+  }
 }
-console.log("docuemtn cookie: ",document.cookie)
+console.log("docuemtn cookie: ", document.cookie);
