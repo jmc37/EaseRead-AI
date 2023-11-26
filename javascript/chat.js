f980203806e0f9252c29245ad5032ec3845ac4d0
B-Rabbit7 <snouri3@my.bcit.ca>
2023-11-25 20:51:13 -0800

    took out access_token string

--- javascript/chat.js
+++ javascript/chat.js
@@ -12,45 +12,45 @@ window.addEventListener(load, function () {
 async function submitForm(event) {
     event.preventDefault();
     console.log("Form was submitted")
-    // let question = document.getElementById("question").value;
+    let question = document.getElementById("question").value;
 
-    // // Create the request data object with user input
-    // let requestData = {
-    //     question: question,
-    // };
+    // Create the request data object with user input
+    let requestData = {
+        question: question,
+    };
 
-    // // Display the loading Hamster spinner
-    // document.getElementById("result").innerHTML = loading_wheel;
+    // Display the loading Hamster spinner
+    document.getElementById("result").innerHTML = loading_wheel;
 
-    // try {
-    //     console.log("sending req")
-    //     // Make the POST request to your Flask backend
-    //     const response = await fetch(chat_route, {
-    //         method: post_method,
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //         },
-    //         body: JSON.stringify(requestData),
-    //     });
+    try {
+        console.log("sending req")
+        // Make the POST request to your Flask backend
+        const response = await fetch(chat_route, {
+            method: post_method,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(requestData),
+        });
 
-    //     // Check if the request was successful (status code 200)
-    //     if (response.ok) {
-    //         const result = await response.json();
+        // Check if the request was successful (status code 200)
+        if (response.ok) {
+            const result = await response.json();
 
-    //         // Display the result in the frontend
-    //         const resultContainer = document.getElementById("result");
+            // Display the result in the frontend
+            const resultContainer = document.getElementById("result");
 
-    //         resultContainer.innerHTML = result.answer;
+            resultContainer.innerHTML = result.answer;
 
-    //         // Show the result container
-    //         document.getElementById("resultContainer").classList.remove('hidden');
-    //     } else {
-    //         // Handle non-successful response (e.g., show an error message)
-    //         console.error(processing_error, response.statusText);
-    //     }
-    // } catch (error) {
-    //     console.error(processing_error, error);
-    //     // Handle processing error, e.g., show an error message
-    // }
+            // Show the result container
+            document.getElementById("resultContainer").classList.remove('hidden');
+        } else {
+            // Handle non-successful response (e.g., show an error message)
+            console.error(processing_error, response.statusText);
+        }
+    } catch (error) {
+        console.error(processing_error, error);
+        // Handle processing error, e.g., show an error message
+    }
 }
 // console.log(document.cookie)
